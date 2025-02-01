cc11dba5d28c687461a56c854fb581366e5739ff
xabigarrido <xabigarrido@gmail.com>
2025-02-02 00:22:56 +0100

    ee

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [user, setUser] = useState([]);
+  const API = "http://localhost:3000";
+  const obtenJson = async () => {
+    const res = await axios.get(API);
+  };
+  const obtenCookie = async () => {
+    const res = await axios.get(`${API}/setcookie`, {
+      withCredentials: true,
+    });
+    console.log(res);
+  };
+  const verCookie = async () => {
+    const res = await axios.get(`${API}/getcookie`, {
+      withCredentials: true,
+    });
+    console.log(res);
+  };
+  const eliminarCookie = async () => {
+    const res = await axios.get(`${API}/deletecookie`, {
+      withCredentials: true,
+    });
+    console.log(res);
+  };
+
+  useEffect(() => {
+    // obtenJson();
+  });
+  return (
+    <div>
+      <button onClick={obtenCookie}>Obtener</button>
+      <button onClick={verCookie}>Ver</button>
+      <button onClick={eliminarCookie}>Eliminar</button>
+    </div>
+  );
+}
+
+export default App;
